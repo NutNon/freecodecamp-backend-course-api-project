8774a8b1a007d4526ed6e339f2ad990574b83355
nutnon <nuttapol.nonsee@gmail.com>
2016-12-26 17:22:36 +0000

    Finish timestamp API.

--- timestamp/server.js
+++ timestamp/server.js
@@ -1,8 +1,21 @@
-var express =   require("express")
+var express =   require(`express`)
+var moment	=	require(`moment`)
 var app = express()
 
 app.get('/', function(req, res) {
     res.send('Hello World!')
+})
+
+app.get('/:time', function(req, res) {
+	// Parse date with moment.js
+    var time = moment(req.params.time, ["X", "MMMM DD, YYYY"])
+    
+    var outpTime = {
+    	unix: time.isValid() ? time.format(`X`) : null,
+    	natural: time.isValid() ? time.format(`MMMM DD, YYYY`) : null,
+    }
+    
+    res.send( JSON.stringify(outpTime));
 })
 
 app.listen(8080, function() {
